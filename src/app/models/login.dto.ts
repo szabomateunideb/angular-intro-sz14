5cb47b33c511952f94b10f48d00e8dcb01ad0c64
szabomateunideb <szabo.mate@inf.unideb.hu>
2025-11-26 15:05:44 +0100

    auth service

---
+++ src/app/models/login.dto.ts
@@ -0,0 +1,4 @@
+export interface LoginDto {
+  felhasznalonev: string;
+  jelszo: string;
+}
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import {BehaviorSubject, tap} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import {LoginDto} from '../models/login.dto';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private apiUrl = 'http://localhost:9090/auth';
+
+  public isLoggedIn$ =
+    new BehaviorSubject<boolean>(this.hasToken());
+
+  constructor(private http: HttpClient) { }
+
+  login(loginDto: LoginDto) {
+    this
+      .http
+      .post(`${this.apiUrl}/bejelentkezes`, loginDto, {responseType: "text"})
+      .pipe(
+        tap(token => {
+          localStorage.setItem('token', token);
+          this.isLoggedIn$.next(true);
+        })
+      );
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.isLoggedIn$.next(false);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem('token');
+  }
+
+  private hasToken(): boolean {
+    return !!localStorage.getItem('token');
+  }
+}
